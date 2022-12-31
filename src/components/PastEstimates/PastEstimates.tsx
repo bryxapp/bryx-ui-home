b588adbf64a0a089f2f0447262ed3d832c8872a5
Bryan Thomas <thomas.bryan.m@gmail.com>
2022-12-30 18:33:27 -0800

    Add Skeleton Pages (#1)

---
+++ src/components/PastEstimates/PastEstimates.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export const PastEstimates = () => {
+    //STATE VARIABLES
+    const [estimatesState, setEstimatesState] = useState({});
+
+    return (
+        <div>
+            <h1>Past Estimates</h1>
+            <br />
+            <h2>TBD</h2>
+        </div>
+    );
+}
