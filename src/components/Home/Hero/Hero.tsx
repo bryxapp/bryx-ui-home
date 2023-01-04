4bc2c9563b333a5af687a08a85c35facc225756d
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-01-03 21:27:46 -0800

    spacing

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -4,7 +4,6 @@ export const Hero = () => {
     return (
         <div className={styles.hero}>
             <div className={styles.hero__content}>
-                <h1 className={styles.hero__title}>Welcome to <span className={styles.hero__title__span}>BRYX</span></h1>
                 <p className={styles.hero__text}>Transforming the boring spreadsheet quotes into visually stunning proposals</p>
                 <button className={styles.hero__button}>Get Started</button>
             </div>
