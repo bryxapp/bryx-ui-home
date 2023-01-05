d516d272f89b308731b594a3f2f16503ae2a6bed
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-01-04 17:30:00 -0800

    Update GitHub Action

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -5,9 +5,8 @@ export const Hero = () => {
         <div className={styles.hero}>
             <div className={styles.hero__content}>
                 <p className={styles.hero__text}>Transforming the boring spreadsheet quotes into visually stunning proposals</p>
-                <button className={styles.hero__button}>Get Started</button>
+                <button className={styles.hero__button}>Get Started!</button>
             </div>
         </div>
-
     );
 };
