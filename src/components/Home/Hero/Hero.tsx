02a93a502b7a597094a003c318e2842b06341bdb
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-01-20 12:07:01 -0800

    Add hero image

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,13 +1,18 @@
-import { Button, Container, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
+import HeroImage from '../../../assets/images/hero.jpg';
+import { Grid } from '@mui/material';
+import Container from '@mui/material/Container';
 
-    /* background: url(../../assets/images/hero.jpg) no-repeat center center; */
 
 const HeroContainer = styled('div')`
+    background: url(${HeroImage}) no-repeat center center;
     background-size: cover;
     margin: 2rem;
     color: #808080;
     text-align: center;
+    height: 40vh;
 `;
 
 const HeroButton = styled(Button)`
@@ -16,21 +21,28 @@ const HeroButton = styled(Button)`
     padding: 1rem;
 `;
 
-
-export const Hero = () => {
+const Hero = () => {
     return (
-        <Container>
-            <HeroContainer>
-                <Typography variant="h2" fontWeight="bold">
-                    Transform Your Quotes
-                </Typography>
-                <Typography variant="h3">
-                    Impress your customers with professional and visually stunning quotes
-                </Typography>
-                <HeroButton href="https://dashboard.bryxapp.com/" variant="contained" color="secondary">
-                    Get Started
-                </HeroButton>
-            </HeroContainer>
-        </Container>
+        <HeroContainer>
+            <div style={{ height: "100%", width: "100%", backgroundColor: "rgba(0,0,0,0.5)" }}>
+                <div style={{ height: '10vh' }} />
+
+                <Grid container direction="row" justifyContent={"flex-end"} alignItems="center" >
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h2" fontWeight="bold" color={"white"}>
+                            Transform Your Quotes
+                        </Typography>
+                        <Typography variant="h3" color={"white"}>
+                            Impress your customers with professional and visually stunning quotes
+                        </Typography>
+                        <HeroButton href="https://dashboard.bryxapp.com/" variant="contained" color="secondary">
+                            Get Started
+                        </HeroButton>
+                    </Grid>
+                </Grid>
+            </div>
+        </HeroContainer>
     );
 };
+
+export default Hero;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Hero } from "./Hero/Hero";
+import Hero from "./Hero/Hero";
 
 const Home = () => {
     return (
